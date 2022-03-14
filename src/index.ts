6c4248f3386fb596be5ae9fb7014da0c82b25e48
Vitaly Turovsky <vital2580@icloud.com>
2022-03-15 00:47:47 +0300

    feat: assignDefined feat: findByFields

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,23 @@ export const oneOf = <T, K extends T>(value: T, ...values: [K, ...K[]]): value i
 
 export const ensureArray = <T>(arg: T | T[]): T[] => (Array.isArray(arg) ? arg : [arg])
 
+const filterObjUndefined = (obj: Record<string, any>) => Object.fromEntries(Object.entries(obj).filter(([, value]) => value !== undefined))
+
+export function assignDefined<T extends Record<string, any>, K extends Partial<T>>(
+    target: T,
+    properties: K,
+): asserts target is T & typeof properties {
+    Object.assign(target, filterObjUndefined(properties))
+}
+
+/**
+ * @param fields `undefined` values will be ignored!
+ */
+export const findByFields = <T extends Record<string, any>>(arr: T[], fields: Partial<T>) => {
+    const filteredFields = filterObjUndefined(fields)
+    return arr.find(item => Object.entries(filteredFields).every(([prop, value]) => item[prop] === value))
+}
+
 /** @throws if not expected */
 export const ensureType = (expectedType: RambdaTypes, name?: string) => {
     const actualType = type(expectedType)
